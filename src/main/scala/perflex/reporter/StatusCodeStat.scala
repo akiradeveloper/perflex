18e0a89bc6d1347fe6dd2c12c9239ed8b41fe32f
Akira Hayakawa <ruby.wktk@gmail.com>
2015-10-20 21:18:17 +0900

    impl StatusCodeStat
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/perflex/reporter/StatusCodeStat.scala
+++ src/main/scala/perflex/reporter/StatusCodeStat.scala
@@ -1,7 +1,20 @@
 package perflex.reporter
 
 import perflex.statkind._
+import scala.collection.mutable
 
 class StatusCodeStat[K <: StatusCode] extends Reporter[K] {
-  override def report(result: Seq[Option[K]]): String = "statusCode"
+  override def report(result: Seq[Option[K]]): String = {
+    val m = new mutable.HashMap[Int, Int]
+    result.foreach {
+      case Some(a) => m(a.statusCode) = m.getOrElse(a.statusCode, 0) + 1
+      case _ => {}
+    }
+    val bui = new StringBuilder
+    bui.append("StatusCode stat:\n")
+    m.toList.sortBy(_._1).foreach { case (k, v) =>
+      bui.append(s"  ${k}[${v}]\n")
+    }
+    bui.result
+  }
 }

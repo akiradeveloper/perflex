dc7d4653acc6c702bb19b587fceb2cdd17eb584e
Akira Hayakawa <ruby.wktk@gmail.com>
2015-10-21 14:50:33 +0900

    add preflex.app package
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- build.sbt
+++ build.sbt
@@ -3,4 +3,11 @@ name := "perflex"
 version := "1.0"
 
 scalaVersion := "2.11.7"
-    
+
+libraryDependencies ++= {
+  Seq(
+    "com.amazonaws" % "aws-java-sdk" % "1.10.12",
+    "com.google.guava" % "guava" % "12.0",
+    "com.github.scopt" %% "scopt" % "3.3.0"
+  )
+}
--- src/main/scala/perflex/app/Fake.scala
+++ src/main/scala/perflex/app/Fake.scala
@@ -2,19 +2,21 @@ import perflex.{ReportMaker, Runner, _}
 
 import scala.util.Random
 
-object MainTest extends App {
+object Fake extends App {
 
   case class MyType(time: Float, statusCode: Int) extends Object
   with statkind.Time
   with statkind.StatusCode
 
-  val tasks = Stream.fill(1000)(Random.nextInt % 1000).map(a => (_:Any) => MyType(a.abs, 200)) ++ Stream(
-    (_: Any) => MyType(111, 200),
-    (_: Any) => MyType(222, 403),
-    (_: Any) => MyType(2, 403),
-    (_: Any) => MyType(3, 403),
-    (_: Any) => { assert(false); MyType(333, 200) }
-  )
+  val tasks =
+    Stream.fill(1000)(Random.nextInt % 1000).map(a => (_:Any) => MyType(a.abs, 200)) ++
+    Stream(
+      (_: Any) => MyType(111, 201),
+      (_: Any) => MyType(222, 403),
+      (_: Any) => MyType(2, 403),
+      (_: Any) => MyType(3, 403),
+      (_: Any) => { assert(false); MyType(333, 200) }
+    )
 
   val result = new Runner(tasks)
     .concurrentNumber(8)

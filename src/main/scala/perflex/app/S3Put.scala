fed9976ef5a40314eb239647a6f0b00b51e89a9b
Akira Hayakawa <ruby.wktk@gmail.com>
2015-10-21 16:23:26 +0900

    add S3Put
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/perflex/app/S3Put.scala
+++ src/main/scala/perflex/app/S3Put.scala
@@ -1,5 +1,7 @@
 package perflex.app
 
+import java.io.{File, InputStream, ByteArrayInputStream}
+import java.nio.file.{Paths, Path, Files}
 import java.util.concurrent.TimeUnit
 
 import com.amazonaws.ClientConfiguration
@@ -9,26 +11,35 @@ import com.google.common.base.Stopwatch
 import perflex.{reporter, ReportMaker, Runner}
 import perflex.statkind.Time
 
+import scala.util.Random
 
 // Given:
-// A file in a bucket
+// An empty bucket
 
-object S3Get extends App {
+object S3Put extends App {
   case class Config(endpoint: String = "",
                     bucketName: String = "",
-                    keyName: String = "",
                     accessKey: String = "BCDEFGHIJKLMNOPQRSTA", // my default
-                    secretKey: String = "bcdefghijklmnopqrstuvwxyzabcdefghijklmna")
+                    secretKey: String = "bcdefghijklmnopqrstuvwxyzabcdefghijklmna",
+                    fileSize: Int = 4096)
 
   val parser = new scopt.OptionParser[Config]("MakeUser") {
     arg[String]("<endpoint>") action { (x, c) => c.copy(endpoint = x) } text("e.g. http://localhost:8080")
     arg[String]("<bucketName>") action { (x, c) => c.copy(bucketName = x) }
-    arg[String]("<keyName>") action { (x, c) => c.copy(keyName = x) }
     opt[String]("accessKey") action { (x, c) => c.copy(accessKey = x) }
     opt[String]("secretKey") action { (x, c) => c.copy(secretKey = x) }
+    opt[Int]("fileSize") action { (x, c) => c.copy(fileSize = x) }
   }
   def run(config: Config) {
     case class Type(time: Float) extends Time
+
+    // make a random file
+    val tmpFileName = "/tmp/perflex-file"
+    Files.deleteIfExists(Paths.get(tmpFileName))
+    val p = Files.createFile(Paths.get(tmpFileName))
+    val bytes: Array[Byte] = Array.fill(config.fileSize)(127)
+    Files.write(p, bytes)
+    val file: File = p.toFile
 
     val tasks = Stream.fill(10000) { (_:Any) =>
       val conf = new ClientConfiguration
@@ -36,8 +47,11 @@ object S3Get extends App {
       val cli = new AmazonS3Client(new BasicAWSCredentials(config.accessKey, config.secretKey), conf)
       cli.setEndpoint(config.endpoint)
       cli.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true))
+      // randname
+      val name = Random.alphanumeric.take(32).mkString
+
       val sw = Stopwatch.createStarted
-      cli.getObject(config.bucketName, config.keyName)
+      cli.putObject(config.bucketName, name, file)
       sw.stop
 
       Type(sw.elapsed(TimeUnit.MICROSECONDS).toFloat)
